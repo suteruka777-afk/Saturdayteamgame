ab0ec63e00ddbe1c8eaf29efcdd5c352e0424e73
しょぽん <naminekana.shopon4646@gmail.com>
2026-07-04 17:50:07 +0900

    リザルトシーン遷移と、スコア表示実装

--- Assets/Scripts/ResultManager.cs
+++ Assets/Scripts/ResultManager.cs
@@ -1,14 +1,38 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ResultManager : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private TextMeshProUGUI _killText;
+    [SerializeField] private TextMeshProUGUI _progText;
+    [SerializeField] private ScoreData _scoreData;
+
     void Start()
     {
-        
+        UpdateText();
     }
 
     void Update()
     {
         
+    }
+
+    private void UpdateText()
+    {
+        _scoreText.text = "Score : " + _scoreData.score.ToString("N0");
+        _killText.text = "Kill : " + _scoreData.KillCount.ToString("N0");
+        if(_scoreData.stageProgress > 100f) _scoreData.stageProgress = 100f;
+        _progText.text = "Stage : " + _scoreData.stageProgress.ToString("N0") +"%";
+    }
+
+    public void ToTitle()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+    public void ReTry()
+    {
+        SceneManager.LoadScene("MainGameScene");
     }
 }
--- Assets/Scripts/ScoreData.cs
+++ Assets/Scripts/ScoreData.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ScoreData", menuName = "Scriptable Objects/ScoreData")]
 public class ScoreData : ScriptableObject
 {
+    [Header("スコア関係の数値")]
     public float score;//スコア
     public int KillCount;//撃破数
     public float stageProgress;//ステージの進捗率
+    public bool isClear;
 
     public void SetUp()
     {
         score = 0f;
         stageProgress = 0f;
+        isClear = false;
     }
 }
